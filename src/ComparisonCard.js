5bbc80bbff26bd177f190e5142c56abe14b2b7ae
Rufus Welsh <rufusasterisk@gmail.com>
2017-10-09 12:21:55 -0600

    Correct line length linter error

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import comparisoncardcss from './styles/ComparisonCard.css';
 
-const ComparisonCard = ( { locationOne, locationTwo, averageOne, averageTwo, comparisonData} ) => (
+const ComparisonCard = ( 
+  { locationOne, locationTwo, averageOne, averageTwo, comparisonData}
+) => (
   <article className='comparison-card'>
     <h3 className='location-one'>{locationOne}</h3>
     <h4 className='average-one'>Average: {averageOne}</h4>
